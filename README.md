454a72f55c7df96ebdd97756d09d32d3738fae47
Cel44 <celineoei04@gmail.com>
2026-05-30 17:28:42 +0800

    style (footer): added page owner info and credits on footer

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Portfolio
 My Portfolio
 
+## Credit
+Art by [purepageart](https://pixabay.com/vectors/ghost-spooky-monster-halloween-9081041/)
+
+## Link
 [Click Here](https://my-portfolio-1upofx636-cel44s-projects.vercel.app/) or Copy : my-portfolio-1upofx636-cel44s-projects.vercel.app/
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,7 +1,10 @@
 export default function Footer() {
    return (
       <footer className="bg-black py-6">
-         <div className="max-w-5xl mx-auto px-6 text-center text-sm text-white">© 2026 Your Name — All Rights Reserved</div>
+         <div className="max-w-5xl mx-auto px-6 text-center text-sm text-white">© 2026 Celine — All Rights Reserved</div>
+         <div className="max-w-5xl mx-auto px-6 text-center text-sm text-white">Art used from: <a href ="https://pixabay.com/vectors/ghost-spooky-monster-halloween-9081041//" title="avatar.png" className="underline hover:text-purple-400"> purepageart</a> on <a href="https://pixabay.com" title="pixabay" className="underline hover:text-green-500"> pixabay </a> 
+         </div>
+
       </footer>
    )
 }
